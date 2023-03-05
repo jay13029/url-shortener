fe340b2d5874a113065e0ed595b5aa183e1d8ff3
jay13029 <jay13029@gmail.com>
2023-03-05 14:34:00 +0800

    Save and find mongoose data

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser')
 const Record = require('./models/record')
 const funcs = require('./config/functions')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
-
-require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
@@ -15,47 +14,55 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
-//首頁
+// 首頁
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-//網址送出
+// 網址送出
 app.post('/', (req, res) => {
+  if (!req.body.url) return res.redirect("/")
   const originUrl = req.body.url
-  // 取得已存code
-  let exitsCodes = []
-  return Record.find({},
-    (err, records) => {
-      if (err) {
-        console.log(err);
-      } else {
-        records.forEach(record => {
-          exitsCodes.push(record.shortCode);
-        });
+  Record.findOne({originUrl})
+    .then(record => {
+      if(!record) { // 不存在，建立新的隨機碼
+        return Record.find()
+        .lean()
+        .then(records => {
+          let exitsCodes = [] //使用過的隨機碼
+          records.forEach(record => {
+            exitsCodes.push(record.shortCode)
+          })
+          // 獲取隨機碼
+          shortCode = funcs.getCode(exitsCodes)
+          Record.create({ originUrl, shortCode })
+          return shortCode
+        })
+      } else { // 資料存在，返回原隨機碼
+        return record.shortCode
       }
     })
-    .then(() => {
-      const shortCode = funcs.getCode(exitsCodes)
+    .then(shortCode => {
       const shortUrl = `${req.protocol}://${req.hostname}:${req.socket.localPort}/${shortCode}`
-      Record.create({ originUrl, shortCode })
       res.render('show', { shortUrl })
     })
-    .catch(error => console.log(error))
+    .catch(err => console.log(err))
 })
 
-//使用短網址
+// 使用短網址
 app.get('/:code', (req, res) => {
   const code = req.params.code
-  //找原網址
-  if (code === 'ABCDE') {
-    res.redirect("http://www.google.com")
-  } else {
-    //查無資料導向錯誤頁面
-    res.render('wrong')
-  }
+  // 找原網址
+  Record.findOne({ shortCode: code })
+    .then(record => {
+      if(!record){ // 查無資料，導向錯誤頁面
+        res.render('wrong')
+      } else {
+        res.redirect(record.originUrl)
+      }
+    })
+    .catch(err => console.error(err))
 })
-
 
 app.listen(port, () => {
   console.log(`This app is listening on http://localhost:${port}`)
